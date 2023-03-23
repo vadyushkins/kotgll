b490729e66bee75d467ade55edd3aecd4fbb1ab6
vadyushkins <vadim.i.abzalov@gmail.com>
2023-03-23 12:03:47 +0300

    Remove DescriptorsQueue.Configuration

--- src/main/kotlin/org/kotgll/DescriptorsQueue.kt
+++ src/main/kotlin/org/kotgll/DescriptorsQueue.kt
@@ -6,13 +6,13 @@ import kotlin.collections.ArrayDeque
 
 class DescriptorsQueue(size: Int) {
     val todo: ArrayDeque<Descriptor> = ArrayDeque()
-    val done: Array<HashSet<Configuration>> = Array(size) { HashSet() }
+    val done: Array<HashSet<Descriptor>> = Array(size) { HashSet() }
 
     fun add(parser: Parser, gssNode: GSSNode, pos: Int, sppfNode: SPPFNode?) {
-        val config = Configuration(parser, gssNode, sppfNode)
-        if (!done[pos].contains(config)) {
-            done[pos].add(config)
-            todo.add(Descriptor(config, pos))
+        val descriptor = Descriptor(parser, gssNode, sppfNode, pos)
+        if (!done[pos].contains(descriptor)) {
+            done[pos].add(descriptor)
+            todo.add(descriptor)
         }
     }
 
@@ -20,51 +20,34 @@ class DescriptorsQueue(size: Int) {
 
     fun isEmpty() = todo.isEmpty()
 
-    class Configuration(
+    class Descriptor(
         val parser: Parser,
         val gssNode: GSSNode,
         val sppfNode: SPPFNode?,
+        val pos: Int,
     ) {
-        override fun toString() = "Configuration(" +
+        override fun toString() = "Descriptor(" +
                 "parser=$parser, " +
                 "gssNode=$gssNode, " +
-                "sppfNode=$sppfNode)"
-
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (other !is Configuration) return false
-
-            if (parser != other.parser) return false
-            if (gssNode != other.gssNode) return false
-            if (sppfNode != other.sppfNode) return false
-
-            return true
-        }
-
-        override fun hashCode() = Objects.hash(parser, gssNode, sppfNode)
-
-        fun parse(pos: Int, driver: GLL) {
-            parser.parse(pos, gssNode, sppfNode, driver)
-        }
-    }
-
-    class Descriptor(val config: Configuration, val pos: Int) {
-        override fun toString() = "Descriptor(config=$config, pos=$pos)"
+                "sppfNode=$sppfNode, " +
+                "pos=$pos)"
 
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
             if (other !is Descriptor) return false
 
-            if (config != other.config) return false
+            if (parser != other.parser) return false
+            if (gssNode != other.gssNode) return false
+            if (sppfNode != other.sppfNode) return false
             if (pos != other.pos) return false
 
             return true
         }
 
-        override fun hashCode() = Objects.hash(config, pos)
+        override fun hashCode() = Objects.hash(parser, gssNode, sppfNode, pos)
 
         fun parse(driver: GLL) {
-            config.parse(pos, driver)
+            parser.parse(pos, gssNode, sppfNode, driver)
         }
     }
 
