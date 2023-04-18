086efff7bf5ffb8cea9f54d25c411d55f83ebc73
vadyushkins <vadim.i.abzalov@gmail.com>
2023-04-18 16:59:56 +0300

    add jar build

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 group = "me.vadyushkins"
 
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories { mavenCentral() }
 
@@ -21,19 +21,13 @@ kotlin { jvmToolchain(11) }
 
 application { mainClass.set("org.kotgll.MainKt") }
 
-tasks {
-  val fatJar = register<Jar>("fatJar") {
-    dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
-    archiveClassifier.set("standalone") // Naming the jar
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    manifest { attributes(mapOf("Main-Class" to application.mainClass)) } // Provided we set it up in the application plugin configuration
-    val sourcesMain = sourceSets.main.get()
-    val contents = configurations.runtimeClasspath.get()
-      .map { if (it.isDirectory) it else zipTree(it) } +
-        sourcesMain.output
-    from(contents)
-  }
-  build {
-    dependsOn(fatJar) // Trigger fat jar creation during build
-  }
+tasks.withType<Jar> {
+  dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+  manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
+  val sourcesMain = sourceSets.main.get()
+  val contents =
+      configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) } +
+          sourcesMain.output
+  from(contents)
 }
